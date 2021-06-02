c84326fbac9037efc95012424f82b5dca70c0040
Daniel <elrion018@gmail.com>
2021-06-03 07:58:11 +0900

    [feature] 축소 기능 구현

--- src/components/LineGraph.vue
+++ src/components/LineGraph.vue
@@ -174,16 +174,16 @@ export default {
       if (event.targetTouches.length === 1) {
         //
       } else if (event.targetTouches.length === 2) {
-        let touches = event.targetTouches;
+        const touches = event.targetTouches;
 
         touches.forEach(touch => {
           this.tpCache.push(touch);
         });
 
-        let pointIndex1 = Math.round(
+        const pointIndex1 = Math.round(
           (touches[0].clientX - this.graphBoxMargin) / this.unitWidth
         );
-        let pointIndex2 = Math.round(
+        const pointIndex2 = Math.round(
           (touches[1].clientX - this.graphBoxMargin) / this.unitWidth
         );
 
@@ -191,12 +191,6 @@ export default {
           Math.min(pointIndex1, pointIndex2) + this.startIndex;
         this.targetEndIndex =
           Math.max(pointIndex1, pointIndex2) + this.startIndex;
-
-        console.log(
-          this.targetStartIndex,
-          this.targetEndIndex,
-          'start, end target index'
-        );
       }
     },
 
@@ -211,6 +205,7 @@ export default {
 
       if (event.targetTouches.length === 0) {
         console.log('touch end');
+
         this.baseStartIndex = this.startIndex;
         this.baseEndIdnex = this.endIndex;
         this.targetStartIndex = null;
@@ -224,58 +219,85 @@ export default {
         event.targetTouches.length === 2 &&
         event.changedTouches.length === 2
       ) {
-        let basePoint1 = -1;
-        let basePoint2 = -1;
+        let leftBasePointClientX = -1;
+        let rightBasePointClientX = -1;
+        let leftBasePoint = -1;
+        let rightBasePoint = -1;
 
         for (let i = 0; i < this.tpCache.length; i++) {
           if (
             this.tpCache[i].identifier === event.targetTouches[0].identifier
           ) {
-            basePoint1 = i;
+            leftBasePoint = i;
+            leftBasePointClientX = this.tpCache[i].clientX;
           }
 
           if (
             this.tpCache[i].identifier === event.targetTouches[1].identifier
           ) {
-            basePoint2 = i;
+            rightBasePoint = i;
+            rightBasePointClientX = this.tpCache[i].clientX;
           }
         }
 
-        if (
-          basePoint1 >= 0 &&
-          basePoint2 >= 0 &&
-          this.endIndex - this.startIndex > 5
-        ) {
-          const diff1 = Math.abs(
-            this.tpCache[basePoint1].clientX - event.targetTouches[0].clientX
+        if (leftBasePointClientX > rightBasePointClientX) {
+          const tempPoint = leftBasePoint;
+          leftBasePoint = rightBasePoint;
+          rightBasePoint = tempPoint;
+        }
+
+        if (leftBasePoint >= 0 && rightBasePoint >= 0) {
+          const leftDiff =
+            this.tpCache[leftBasePoint].clientX -
+            event.targetTouches[leftBasePoint].clientX;
+
+          const rightDiff =
+            this.tpCache[rightBasePoint].clientX -
+            event.targetTouches[rightBasePoint].clientX;
+
+          const totalDiff = Math.round(
+            Math.abs(leftDiff) + Math.abs(rightDiff)
           );
 
-          const diff2 = Math.abs(
-            this.tpCache[basePoint2].clientX - event.targetTouches[1].clientX
-          );
+          const diffIndex = parseInt(totalDiff / 20);
 
-          const totalDiff = Math.round(diff1 + diff2);
+          if (
+            leftDiff > 0 &&
+            rightDiff < 0 &&
+            this.endIndex - this.startIndex > 5
+          ) {
+            if (this.startIndex < this.targetStartIndex) {
+              if (this.baseStartIndex + diffIndex > this.targetStartIndex) {
+                this.startIndex = this.targetStartIndex;
+              } else {
+                this.startIndex = this.baseStartIndex + diffIndex;
+              }
+            }
 
-          console.log(totalDiff, 'totalDiff');
-          const diffIndex = parseInt(totalDiff / 15);
+            if (this.endIndex > this.targetEndIndex) {
+              if (this.baseEndIdnex - diffIndex < this.targetEndIndex) {
+                this.endIndex = this.targetEndIndex;
+              } else {
+                this.endIndex = this.baseEndIdnex - diffIndex;
+              }
+            }
+          } else if (leftDiff < 0 && rightDiff > 0) {
+            if (this.startIndex > 0) {
+              if (this.baseStartIndex - diffIndex < 0) {
+                this.startIndex = 0;
+              } else {
+                this.startIndex = this.baseStartIndex - diffIndex;
+              }
+            }
 
-          if (this.startIndex < this.targetStartIndex) {
-            if (this.baseStartIndex + diffIndex > this.targetStartIndex) {
-              this.startIndex = this.targetStartIndex;
-            } else {
-              this.startIndex = this.baseStartIndex + diffIndex;
+            if (this.endIndex < this.data.length - 1) {
+              if (this.baseEndIdnex + diffIndex > this.data.length - 1) {
+                this.endIndex = this.data.length - 1;
+              } else {
+                this.endIndex = this.baseEndIdnex + diffIndex;
+              }
             }
           }
-
-          if (this.endIndex > this.targetEndIndex) {
-            if (this.baseEndIdnex - diffIndex < this.targetEndIndex) {
-              this.endIndex = this.targetEndIndex;
-            } else {
-              this.endIndex = this.baseEndIdnex - diffIndex;
-            }
-          }
-
-          console.log(this.startIndex, this.endIndex);
         }
       }
     },
